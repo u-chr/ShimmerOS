2b5ddc200efaa686034e704daba2dbe859c5a432
loplxl <lop.crystallisedsatisfaction.lxl@proton.me>
2026-02-03 02:28:30 +0000

    fix accidental icon2

--- Source Code/build.bat
+++ Source Code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 rmdir /s /q "dist/Software"
-pyinstaller --noconfirm --onedir --windowed --icon="assets\icon2.ico" --add-data "assets;assets" --add-data "TimerResolution;TimerResolution" --add-data "downloads;downloads" --add-data "tools;tools" --add-data "dependencies;dependencies" --hidden-import bs4 --hidden-import win32com --hidden-import psutil main.py --name Shimmer --noconfirm
+pyinstaller --noconfirm --onedir --windowed --icon="assets\icon.ico" --add-data "assets;assets" --add-data "TimerResolution;TimerResolution" --add-data "downloads;downloads" --add-data "tools;tools" --add-data "dependencies;dependencies" --hidden-import bs4 --hidden-import win32com --hidden-import psutil main.py --name Shimmer --noconfirm
 ren "dist/Shimmer" Software
 timeout /t 1 /nobreak >nul
 powershell -Command Compress-Archive -Path .\dist\Software -DestinationPath .\dist\Shimmer.zip -Update
